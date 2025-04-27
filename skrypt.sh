4b30e106cd837f98872b0e740e84680dcb100c89
Kacperbdsw <dsw59276@student.dsw.edu.pl>
2025-04-27 09:15:58 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+
+case "$1" in
+    --date)
+            echo "Dzisiejsza data: $(date)"
+        ;;
+    --logs)
+        liczba=${2:-100} #Domyslne 100 jesli nie zostnaie podana liczba
+        mkdir -p logs
+        for ((i=1; i<=liczba; i++)); do 
+            filename="logs/log${i}.txt"
+            echo "Nazwa pliku: log${i}.txt" > "$filename"
+            echo "Data utworzenia: $(date)" >> "$filename"
+        done
+        echo "Stworzono 100 plikow"
+        ;;
+    --help)
         echo "Dostępne opcje:"
         echo "  --help -h         Wyświetl pomoc"
         echo "  --init         Sklonuj repo"
         echo "  --date -d       Wyświetl dzisiejszą datę"
         echo "  --logs [liczba] -l    Utwórz pliki logx.txt (domyślnie 100)"
         echo "  --e [liczba] -e    Utwórz pliki error.txt (domyślnie 100)"
-        echo "Stworzono $liczba plikow"
+        ;;
+    *)
+        echo "Nieprawidłowa opcja."
+        ;;
+esac
