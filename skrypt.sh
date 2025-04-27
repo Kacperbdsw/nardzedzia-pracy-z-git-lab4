4769c7d1bd472521d2d298d43978d064bd29788c
Kacperbdsw <dsw59276@student.dsw.edu.pl>
2025-04-27 09:10:30 +0200

    -error

--- skrypt.sh
+++ skrypt.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
 case "$1" in
-    --date)
-            echo "Dzisiejsza data: $(date)"
-        ;;
-    --logs)
-        liczba=${2:-100} #Domyslne 100 jesli nie zostnaie podana liczba
-        mkdir -p logs
-        for ((i=1; i<=liczba; i++)); do 
-            filename="logs/log${i}.txt"
-            echo "Nazwa pliku: log${i}.txt" > "$filename"
-            echo "Data utworzenia: $(date)" >> "$filename"
-        done
-        echo "Stworzono 100 plikow"
-        ;;
-    --help)
-        echo "Dostępne opcje:"
-        echo "  --help          Wyświetl pomoc"
-        echo "  --date          Wyświetl dzisiejszą datę"
-        echo "  --logs [liczba]    Utwórz pliki logx.txt (domyślnie 100)"
-        echo "Stworzono $liczba plikow"
-        ;;
-    *)
-        echo "Nieprawidłowa opcja."
-        ;;
+   --error|-e)
+       liczba=${2:-100}
+       mkdir -p errorx
+       for ((i=1; i<=liczba; i++)); do
+           filename="errorx/error${i}.txt"
+           echo "Nazwa pliku: error${i}.txt" > "$filename"
+           echo "Data utworzenia: $(date)" >> "$filename"
+           echo "Utworzony przez: skrypt.sh" >> "$filename"
+       done
+       echo "Utworzono $liczba plików error."
+       ;;
+   *)
+       echo "Nieprawidłowa opcja."
+       ;;
 esac
