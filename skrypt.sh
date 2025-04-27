005cea9bf76f29e09583b4d819517d8ff32fd46b
Kacperbdsw <dsw59276@student.dsw.edu.pl>
2025-04-27 09:06:25 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 case "$1" in
-    --date)
-            echo "Dzisiejsza data: $(date)"
-        ;;
-    --logs)
-        liczba=${2:-100} #Domyslne 100 jesli nie zostnaie podana liczba
-        mkdir -p logs
-        for ((i=1; i<=liczba; i++)); do 
-            filename="logs/log${i}.txt"
-            echo "Nazwa pliku: log${i}.txt" > "$filename"
-            echo "Data utworzenia: $(date)" >> "$filename"
-        done
-        echo "Stworzono 100 plikow"
-        ;;
-    --help)
-        echo "Dostępne opcje:"
-        echo "  --help          Wyświetl pomoc"
-        echo "  --date          Wyświetl dzisiejszą datę"
-        echo "  --logs [liczba]    Utwórz pliki logx.txt (domyślnie 100)"
-        echo "Stworzono $liczba plikow"
+    --init)
+        git clone https://github.com/Kacperbdsw/nardzedzia-pracy-z-git-lab4.git cloned_repo
+        cd cloned_repo || exit
+        export PATH=$PATH:$(pwd)
+        echo "Repo sklonowane i PATH ustawiony"
         ;;
     *)
         echo "Nieprawidłowa opcja."
